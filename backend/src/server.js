0c493b7b13171cf84dc70a5294b57fb0cd28613c
David Cunningham <david7789@gmail.com>
2022-05-22 16:46:40 -0400

    Added CORS allowance to server.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const { getArticlesListing, getArticle, editArticle, createArticle } = require('./db');
 
@@ -10,66 +11,62 @@ const ARTICLE_ID_PATH = `${ARTICLES_PATH}/:articleId`
 // Express App
 const app = express();
 
-// Get articles listing
-app.get(ARTICLES_PATH, (req, res, next) => {
+// Allow ALL CORS Requests (should update to prod-specific)
+app.use(cors());
 
-    // TODO: fetch articles from postgres
+const articlesRouter = express.Router();
+
+// Get articles listing
+articlesRouter.get(ARTICLES_PATH, (req, res, next) => {
     getArticlesListing()
         .then(rows => {
-            // TODO: return articles data
-
-            return res.json({ data: articlesData })
+            // TODO: remove
+            console.log('GET Articles:');
+            console.log(rows);
+            return res.json({ data: rows })
         })
-        .catch(err => {
-            return res.json({ err })
-        })
-
+        .catch(err => res.json({ err }))
 });
 
 // Get article contents
-app.get(ARTICLE_ID_PATH, (req, res, next) => {
+articlesRouter.get(ARTICLE_ID_PATH, (req, res, next) => {
     const { articleId } = req.params;
-
-    // TODO: fetch article from postgres
     getArticle(articleId)
         .then(rows => {
-
-            return res.json({ data: articleData })
+            return res.json({ data: rows[0] })
         })
         .catch(err => res.json({ err }))
-    // TODO: return article data
-    return res.json(ARTICLE_DATA);
 });
 
 // Edit article
-app.put(ARTICLE_ID_PATH, (req, res, next) => {
+articlesRouter.put(ARTICLE_ID_PATH, (req, res, next) => {
     const { articleId } = req.params;
     const { author, title, markdownContent } = req.body;
     const articleData = { author, title, markdownContent };
 
     editArticle(articleId, articleData)
         .then(sqlResponse => {
-            // TODO: return success/err response
-            
+            return res.json({ data: sqlResponse })
         })
         .catch(err => res.json({ err }))
 });
 
 // New article
-app.post(ARTICLES_PATH, (req, res, next) => {
+articlesRouter.post(ARTICLES_PATH, (req, res, next) => {
     const { articleTitle, articleAuthor, articleContent } = req.body;
     const articleData = { articleTitle, articleAuthor, articleContent };
 
     // TODO: insert in postgres
     createArticle(articleData)
         .then(sqlResponse => {
-            // TODO: return success/err response
+            return res.json({ data: sqlResponse })
         })
         .catch(err => res.json({ err }))
-
-
 });
 
+// Adding Router(s)
+// Adding Router: Articles
+app.use('/api', articlesRouter);
 
 // Listening
 app.listen(LISTEN_PORT, function() {
